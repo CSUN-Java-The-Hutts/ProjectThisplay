fdf2c120102dbd4e2dfe63e0e4d136c873abbcb0
Shreygahl27 <141099924+Shreygahl27@users.noreply.github.com>
2024-05-13 23:17:22 +0530

    Update settings.py

--- ProjectThisplay/Web App/WebAppDjango/ThisPlay/settings.py
+++ ProjectThisplay/Web App/WebAppDjango/ThisPlay/settings.py
@@ -20,7 +20,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Base directory for media uploads
+MEDIA_ROOT = BASE_DIR / 'media'  # Base directory for media uploads
 MEDIA_URL = '/media/'  # URL to access media
 
 # Quick-start development settings - unsuitable for production
