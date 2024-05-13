de6dce3443a7db648e7132b15885cee43bd24a8a
Shreygahl27 <141099924+Shreygahl27@users.noreply.github.com>
2024-05-13 23:15:29 +0530

    Update models.py

--- ProjectThisplay/Web App/WebAppDjango/ThisPlaywebpage/models.py
+++ ProjectThisplay/Web App/WebAppDjango/ThisPlaywebpage/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
 class Uploads(models.Model):
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    file = models.FileField(upload_to='uploads/', null=True, blank=True)
     text_content = models.TextField(null=True, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
